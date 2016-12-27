7261836fba3d5a0159bfe14d99855bfc5092f61f
Chekad Sarami <csarami@users.noreply.github.com>
2016-12-27 10:59:22 -0500

    init commit

---
+++ app/views.py
@@ -0,0 +1,68 @@
+"""
+Definition of views.
+"""
+
+from django.shortcuts import render, render_to_response
+from django.http import HttpRequest
+from django.template import RequestContext
+from datetime import datetime
+from django.http import HttpRequest, HttpResponse,HttpResponseRedirect
+from app.models import *
+
+def artists(request):
+    #return HttpResponse('<html><head><title>Hello, Django! </title></head><body><h1>Hello, Django </h1></body></html>')
+    artists = Artist.objects.all()
+    return render_to_response('app/artists.html', {'artists' : artists})
+def artistdetails(request, id):
+    '''output = '<html><head><title>Hello, '+name
+    output += '</title></head><body><h1>Hello ' +name +'</h1></body></html>'
+    return HttpResponse(output)'''
+    artist = Artist.objects.get(pk = id) # pk is built-in primar key
+    return render_to_response('app/artistdetails.html', {'artist' :artist})
+
+def artistcreate(request):
+    if request.method == 'GET':
+        form = ArtistForm()
+        return render(request, 'app/create.html', {"form":form})
+    elif request.method == 'POST':
+        form = ArtistForm(request.POST)
+        form.save()
+        return HttpResponseRedirect('/artists')
+
+def home(request):
+    """Renders the home page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/index.html',
+        {
+            'title':'Home Page',
+            'year':datetime.now().year,
+        }
+    )
+
+def contact(request):
+    """Renders the contact page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/contact.html',
+        {
+            'title':'Contact',
+            'message':'Your contact page.',
+            'year':datetime.now().year,
+        }
+    )
+
+def about(request):
+    """Renders the about page."""
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/about.html',
+        {
+            'title':'About',
+            'message':'Your application description page.',
+            'year':datetime.now().year,
+        }
+    )
